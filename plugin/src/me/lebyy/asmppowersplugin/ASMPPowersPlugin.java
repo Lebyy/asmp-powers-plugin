22ab62f63c972b7c325ac6c604953495b92f3381
Lebyy <45114019+Lebyy@users.noreply.github.com>
2022-04-01 23:20:46 +0530

    ✨ | Adding new player powers

--- plugin/src/me/lebyy/asmppowersplugin/ASMPPowersPlugin.java
+++ plugin/src/me/lebyy/asmppowersplugin/ASMPPowersPlugin.java
@@ -1,12 +1,19 @@
 package me.lebyy.asmppowersplugin;
 
+import me.lebyy.asmppowersplugin.ArmorListener.ArmorEquipEvent;
+import me.lebyy.asmppowersplugin.ArmorListener.ArmorListener;
+import me.lebyy.asmppowersplugin.ArmorListener.DispenserArmorListener;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,7 +31,13 @@ public class ASMPPowersPlugin extends JavaPlugin implements Listener {
     private static Vector lastBarrierBlockLocation;
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new ArmorListener(getConfig().getStringList("blocked")), this);
+        try{
+            Class.forName("org.bukkit.event.block.BlockDispenseArmorEvent");
+            getServer().getPluginManager().registerEvents(new DispenserArmorListener(), this);
+        }catch(Exception ignored){}
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Artist SMP Powers Plugin]: The plugin was enabled and is ready to rock n roll!");
     }
 
@@ -126,6 +139,16 @@ public class ASMPPowersPlugin extends JavaPlugin implements Listener {
         if (player.hasPermission("powers.ice")) {
             player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0));
         }
+        if(player.hasPermission("powers.milo")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1));
+        }
+        if(player.hasPermission("powers.lake")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, Integer.MAX_VALUE, 5));
+        }
+        if(player.hasPermission("powers.wilbie")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 0));
+        }
     }
 
     // Trigerred once player joins the server.
@@ -134,6 +157,16 @@ public class ASMPPowersPlugin extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         if (player.hasPermission("powers.ice")) {
             player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0));
+        }
+        if(player.hasPermission("powers.milo")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1));
+        }
+        if(player.hasPermission("powers.lake")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, Integer.MAX_VALUE, 5));
+        }
+        if(player.hasPermission("powers.wilbie")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 0));
         }
     }
 
@@ -145,6 +178,62 @@ public class ASMPPowersPlugin extends JavaPlugin implements Listener {
         if (player.hasPermission("powers.ice")) {
             if (food.getType() == Material.MILK_BUCKET) {
                 player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0));
+            }
+        }
+        if(player.hasPermission("powers.milo")) {
+            if (food.getType() == Material.MILK_BUCKET) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1));
+            }
+        }
+        if(player.hasPermission("powers.lake")) {
+            if (food.getType() == Material.MILK_BUCKET) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, Integer.MAX_VALUE, 5));
+            }
+        }
+        if(player.hasPermission("powers.wilbie")) {
+            if (food.getType() == Material.MILK_BUCKET) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 0));
+            }
+        }
+    }
+
+    // Trigerred once an entity targets a player.
+    @EventHandler
+    public void onEntityTargetPlayer(EntityTargetLivingEntityEvent event) {
+        Player player = (Player) event.getTarget();
+        assert player != null;
+        Entity entity = event.getEntity();
+
+        if(player.hasPermission("powers.crossed")) {
+            if(entity.getType() == EntityType.SPIDER) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    // Trigerred once an player wears armor.
+    @EventHandler
+    public void onPlayerEquipArmor(ArmorEquipEvent event) {
+        Player player = event.getPlayer();
+
+        if(player.hasPermission("powers.milo")) {
+            event.setCancelled(true);
+        }
+    }
+
+    // Trigerred once an player armor breaks.
+    @EventHandler
+    public void onPlayerArmorBreak(PlayerItemBreakEvent event) {
+        Player player = event.getPlayer();
+
+        if(player.hasPermission("powers.lake")) {
+            ItemStack item = event.getBrokenItem();
+
+            if (item.getType() == Material.CHAINMAIL_CHESTPLATE) {
+                if (item.getItemMeta().getDisplayName().toLowerCase() == "amethyst heart") {
+                    player.damage(Integer.MAX_VALUE);
+                }
             }
         }
     }
